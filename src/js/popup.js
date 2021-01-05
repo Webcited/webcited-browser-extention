d4411a2bd0768b829110e07da1b0383c8fc4fa45
shivam <shivamgit@pm.me>
2021-01-05 20:34:22 +0530

    Add progress text
    Signed-off-by: shivam <shivamgit@pm.me>

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,9 @@
 import "../css/popup.css";
+
+let progressText = document.getElementById("progress");
+document.addEventListener("submit", () => {
+  progressText.textContent = "Processing...";
+  setTimeout(() => {
+    progressText.textContent = "Finished";
+  }, 5000);
+});
